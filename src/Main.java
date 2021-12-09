3d66196abc437bc00d336f4a064ac3411f659873
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-09 11:56:06 +0200

    Enabled assertions in project configurations. Now if an assertion is wrong an exception is thrown when the project is run.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 
-public class Main
-{
+public class Main {
     private static final double EPSILON = 0.001;
 
     // Note: This method is NOT the same method as the almostEquals method of class ComplexNumber.
-    private static boolean almostEquals(double f1, double f2)
-    {
+    private static boolean almostEquals(double f1, double f2) {
         return Math.abs(f1 - f2) < EPSILON;
     }
 
-    public static void main(String argv[])
-    {
+    public static void main(String argv[]) {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(3, 4);
 
@@ -27,5 +24,8 @@ public class Main
 
         assert almostEquals(z1.divide(z2).getRadius(), z1.getRadius() / z2.getRadius());
         assert almostEquals(z1.divide(z2).getArgument(), z1.getArgument() - z2.getArgument());
+
+
+
     }
 }
