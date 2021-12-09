de932b933b17f265003471c2460e0ef558eda9e1
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-09 12:37:25 +0200

    Added public/private keywords to methods and fields.

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -2,13 +2,13 @@ public class ComplexNumber {
 
     //Data members
 
-    double real;
-    double imaginary;
-    static final ComplexNumber epsilon = new ComplexNumber(0.001, 0);
+    private double real;
+    private double  imaginary;
+    public static final ComplexNumber epsilon = new ComplexNumber(0.001, 0);
 
     //Constructor
 
-    ComplexNumber(double realPart, double imaginaryPart){
+    public ComplexNumber(double realPart, double imaginaryPart){
         real = realPart;
         imaginary = imaginaryPart;
 
@@ -16,47 +16,47 @@ public class ComplexNumber {
 
     //Methods
 
-    ComplexNumber plus(ComplexNumber other) {
+    public ComplexNumber plus(ComplexNumber other) {
         return new ComplexNumber(real + other.real, imaginary + other.imaginary);
     }
 
-    ComplexNumber minus(ComplexNumber other) {
+    public ComplexNumber minus(ComplexNumber other) {
         return new ComplexNumber(real - other.real, imaginary - other.imaginary);
     }
 
-    ComplexNumber times(ComplexNumber other) {
+    public ComplexNumber times(ComplexNumber other) {
         double new_radius = this.getRadius() * other.getRadius();
         double new_argument = this.getArgument() + other.getArgument();
         return fromPolarCoordinates(new_radius, new_argument);
     }
 
-    ComplexNumber divide(ComplexNumber other) {
+    public ComplexNumber divide(ComplexNumber other) {
         double new_radius = this.getRadius() / other.getRadius();
         double new_argument = this.getArgument() - other.getArgument();
         return fromPolarCoordinates(new_radius, new_argument);
     }
 
-    static ComplexNumber fromPolarCoordinates(double radius, double argument) {
+    public static ComplexNumber fromPolarCoordinates(double radius, double argument) {
         return new ComplexNumber(radius*Math.cos(argument), radius*Math.sin(argument));
     }
 
-    double getRealPart() {
+    public double getRealPart() {
         return real;
     }
 
-    double getImaginaryPart() {
+    public double getImaginaryPart() {
         return imaginary;
     }
 
-    double getRadius() {
+    public double getRadius() {
         return Math.sqrt(Math.pow(real, 2) + Math.pow(imaginary, 2));
     }
 
-    double getArgument() {
+    public double getArgument() {
         return Math.atan2(imaginary, real);
     }
 
-    boolean almostEquals(ComplexNumber other) {
+    public boolean almostEquals(ComplexNumber other) {
 
         return this.minus(other).getRadius() < epsilon.getRadius();
 
